f311eff7dc766dcbbd37f85cb565f769c0fa2037
amechi101 <egbe@usc.edu>
2016-07-23 16:10:52 -0400

    bash updated

--- src/sites/sites.sh
+++ src/sites/sites.sh
@@ -2,6 +2,7 @@
 
 ROOT_DIR="$PWD"
 CURRENT_DIR="${BASH_SOURCE%/*}"
+DONE="Finished"
 
-for f in `find $ROOT_DIR/$CURRENT_DIR -name '*.py'`; do python "$f"; done
+for f in `find $ROOT_DIR/$CURRENT_DIR -name '*.py'`; do python "$f"; done; echo $DONE
 
