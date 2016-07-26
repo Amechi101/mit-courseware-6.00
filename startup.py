dbf9b818a57a68ca5f3ec816b30dc534b6b2a1be
amechi101 <egbe@usc.edu>
2016-07-26 14:14:13 -0400

    program updated

--- startup.py
+++ startup.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-from __future__ import print_function # For python 2.x
-
 from src.core.designer_names_scraper import DesignerNamesScraper
 from src.core.designer_names_sorting import DesignerNamesSorting
 
